60d3604bb7748b5d95456e7e6bf3c89fad546be6
Kemas Khairunsyah <khairunsyah2714@gmail.com>
2024-10-31 14:02:53 +0700

    Update SkillCard.jsx

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -3,7 +3,7 @@ import { AnimatedSection } from "./AnimatedSection";
 
 export const SkillCard = ({ skill }) => (
   <AnimatedSection>
-    <div className="bg-card rounded-lg shadow-md p-6 text-center flex flex-col items-center min-h-[12rem] max-h-[14rem] min-w-[16rem] max-w-[18rem] sm:min-w-[12rem] sm:max-w-[14rem] md:min-w-[20rem] md:max-w-[24rem]">
+    <div className="bg-card rounded-lg shadow-md p-6 text-center flex flex-col items-center min-h-[14rem] max-h-[16rem] min-w-[16rem] max-w-[18rem] sm:min-w-[12rem] sm:max-w-[14rem] md:min-w-[20rem] md:max-w-[24rem]">
       {/* Display the skill image */}
       {skill.image && (
         <img
